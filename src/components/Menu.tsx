3d26f7d284dfe381bcd1bf1ec253d79260bcbb3c
0lgaP1 <volhapatapchuk@gmail.com>
2024-06-07 23:44:06 +0200

    Header naming

---
+++ src/components/Menu.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// import {S} from '../Header_Styles';
+
+const items = [
+    {
+        title: "Register now",
+        href: "registration"
+    },
+    {
+        title: "Video library",
+        href: "video"
+    },
+    {
+        title: "Gallery",
+        href: "gallery"
+    },
+    {
+        title: "News",
+        href: "news"
+    },
+    {
+        title: "Shop",
+        href: "shop"
+    },
+    {
+        title: "About us",
+        href: "about"
+    },{
+        title: "Contact",
+        href: "contact"
+    }]
+export const Menu: React.FC = () => {
+    return (
+        <ul>
+            <li></li>
+        </ul>
+    );
+};
